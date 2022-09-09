f3796aace747da72f0cfc98dcb145ecd3ca915ad
Dashiell Bringhurst <dashbringhurst@gmail.com>
2022-09-09 17:00:37 -0500

    edited functions

--- explore.py
+++ explore.py
@@ -52,9 +52,9 @@ def plot_vars(df):
     # third subplot of 9, displayed as 3 rows and 3 columns
     plt.subplot(3,3,3)
     # lineplot is used for 'year' because it was the best visualization for this variable
-    sns.lineplot(x='year', y='tax_value', data=df)
+    sns.boxenplot(x='bed_bath', y='tax_value', data=df, showfliers=False)
     # red regression line overlayed on boxplot, extended to fit the entire x-axis
-    sns.regplot(x='year', y='tax_value', data=df, truncate=False, scatter=False, color='red')
+    sns.regplot(x='bed_bath', y='tax_value', data=df, truncate=False, scatter=False, color='red')
     # the rest of the code is the same as above but the y-variables are changed
     plt.subplot(3,3,4)
     sns.boxenplot(x='bedrooms', y='lot_size', data=df, showfliers=False)
@@ -63,8 +63,8 @@ def plot_vars(df):
     sns.boxenplot(x='bathrooms', y='lot_size', data=df, showfliers=False)
     sns.regplot(x='bathrooms', y='lot_size', data=df, truncate=False, scatter=False, color='red')
     plt.subplot(3,3,6)
-    sns.lineplot(x='year', y='lot_size', data=df)
-    sns.regplot(x='year', y='lot_size', data=df, truncate=False, scatter=False, color='red')
+    sns.boxenplot(x='bed_bath', y='lot_size', data=df, showfliers=False)
+    sns.regplot(x='bed_bath', y='lot_size', data=df, truncate=False, scatter=False, color='red')
     plt.subplot(3,3,7)
     sns.boxenplot(x='bedrooms', y='sqft', data=df, showfliers=False)
     sns.regplot(x='bedrooms', y='sqft', data=df, truncate=False, scatter=False, color='red')
@@ -72,6 +72,60 @@ def plot_vars(df):
     sns.boxenplot(x='bathrooms', y='sqft', data=df, showfliers=False)
     sns.regplot(x='bathrooms', y='sqft', data=df, truncate=False, scatter=False, color='red')
     plt.subplot(3,3,9)
+    sns.boxenplot(x='bed_bath', y='sqft', data=df, showfliers=False)
+    sns.regplot(x='bed_bath', y='sqft', data=df, truncate=False, scatter=False, color='red')
+
+def select_kbest(X, y, k):
+    '''This function takes in three arguments, X (selected features), y (target variable), and k (number of 
+    features to select) and calculates the top features using selectKbest. The function returns a list of the 
+    top features.'''
+    f_selector = SelectKBest(f_regression, k=k)
+    # find the top 2 X's correlated with y
+    f_selector.fit(X, y)
+    # boolean mask of whether the column was selected or not. 
+    feature_mask = f_selector.get_support()
+    # get list of top K features. 
+    f_feature = X.iloc[:,feature_mask].columns.tolist()
+    return f_feature
+
+def rfe(X, y, k):
+    '''This function takes in three arguments, X (selected features), y (target variable), and k (number of 
+    features to select) and calculates the top features using recursive feature elimination. The function
+    returns a list of the top features.'''
+    # initialize the ML algorithm
+    lm = LinearRegression()
+    # create the rfe object, indicating the ML object (lm) and the number of features I want to end up with. 
+    rfe = RFE(lm, n_features_to_select=k)
+    # fit the data using RFE
+    rfe.fit(X,y)  
+    # get the mask of the columns selected
+    feature_mask = rfe.support_
+    # get list of the column names. 
+    rfe_feature = X.iloc[:,feature_mask].columns.tolist()
+    return rfe_feature
+
+def plot_time_vars(df):
+    '''This function takes in a dataframe and returns visualizations for each discrete/continuous variable combination.'''
+    # adjust figure size to make the charts easier to see
+    plt.figure(figsize=[20,16])   
+    # first subplot of 9, displayed as 3 rows and 3 columns
+    plt.subplot(3,3,1)
+    # lineplot is used for 'year' because it was the best visualization for this variable
+    sns.lineplot(x='year', y='tax_value', data=df)
+    # red regression line overlayed on boxplot, extended to fit the entire x-axis
+    sns.regplot(x='year', y='tax_value', data=df, truncate=False, scatter=False, color='red')
+    # the rest of the code is the same as above but the y-variables are changed
+    plt.subplot(3,3,2)
+    sns.lineplot(x='year', y='lot_size', data=df)
+    sns.regplot(x='year', y='lot_size', data=df, truncate=False, scatter=False, color='red')
+    plt.subplot(3,3,3)
     sns.lineplot(x='year', y='sqft', data=df)
     sns.regplot(x='year', y='sqft', data=df, truncate=False, scatter=False, color='red')
-
+    plt.subplot(3,3,4)
+    sns.lineplot(x='year', y='lot_minus_home', data=df)
+    sns.regplot(x='year', y='lot_minus_home', data=df, truncate=False, scatter=False, color='red')
+    plt.subplot(3,3,5)
+    sns.lineplot(x='year', y='bed_bath', data=df)
+    sns.regplot(x='year', y='bed_bath', data=df, truncate=False, scatter=False, color='red')
+    plt.subplot(3,3,6)
+    sns.histplot(x='tax_value', hue='fips', data=df, palette="Blues", multiple="stack")
